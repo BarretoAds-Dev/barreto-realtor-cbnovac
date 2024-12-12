c851b7a16ea181ce01a011267952a48fb473edbb
Jesus Enmanuel Barreto <barretoads@192.168.1.28>
2024-12-12 13:47:53 -0600

    Deploy: 2024-12-12 13:47:53

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -14,5 +12,5 @@ export default defineConfig({
   }),
 
   integrations: [
-    tailwind({ applyBaseStyles: true }), react()]
+    tailwind({ applyBaseStyles: true })]
 });
