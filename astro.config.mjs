6a5fc16a4db261171aaa63162166cf9198a684b7
Jesus Enmanuel Barreto <barretoads@192.168.1.28>
2024-12-14 11:24:35 -0600

    Deploy: 2024-12-14 11:24:35

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,49 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
+import viteCompression from 'vite-plugin-compression';
 
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-  imageService: 'cloudflare',
-  platformProxy: {enabled: true,},
+    imageService: 'cloudflare',
+    platformProxy: { enabled: true },
   }),
-
   integrations: [
-    tailwind({ applyBaseStyles: false }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
   ],
-
-
-
+  vite: {
+    build: {
+      minify: 'esbuild',
+      cssCodeSplit: true,
+      rollupOptions: {
+        treeshake: {
+          moduleSideEffects: false,
+        },
+      },
+    },
+    plugins: [
+      viteCompression({
+        algorithm: 'brotliCompress',
+        ext: '.br',
+        threshold: 0,
+        deleteOriginFile: false,
+        compressionOptions: { level: 11 },
+        filter: /\.(js|mjs|json|css|html|svg|jsx|tsx|astro|txt|xml|mp4|webp|avif|png|jpe?g|gif)$/,
+        verbose: false,
+      }),
+      viteCompression({
+        algorithm: 'gzip',
+        ext: '.gz',
+        threshold: 0,
+        deleteOriginFile: false,
+        compressionOptions: { level: 9 },
+        filter: /\.(js|mjs|json|css|html|svg|jsx|tsx|astro|txt|xml|mp4|webp|avif|png|jpe?g|gif)$/,
+        verbose: false,
+      }),
+    ],
+    cacheDir: '.vite-cache',
+  },
 });
