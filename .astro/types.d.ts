be3650fc442bfbc3a267733908a95ab44047d19f
Jesus Enmanuel Barreto <barretoads@192.168.1.23>
2025-01-13 12:48:15 -0600

    Deploy: 2025-01-13 12:48:15

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="astro/client" />
 /// <reference path="actions.d.ts" />
 /// <reference path="integrations/_inox-tools_astro-when/types.d.ts" />
-/// <reference path="content.d.ts" />
